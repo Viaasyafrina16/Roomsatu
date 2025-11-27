05529bf2f3fc396b53bf8a9e069675c61c8b71b1
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-27 11:20:35 +0700

    menambahkan konfigurasi DatabaseSiswa menggunakan Roo

---
+++ app/src/main/java/com/example/roomsatu/room/DatabaseSiswa.kt
@@ -0,0 +1,28 @@
+package com.example.roomsatu.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomsatu.room.SiswaDao
+import com.example.roomsatu.room.Siswa
+
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa.database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
+}
