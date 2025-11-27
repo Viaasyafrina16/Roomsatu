f5d7c9452ccfcf95e64ba48d838999d0a76f5cbb
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-27 15:33:20 +0700

    buat EntryViewModel untuk mengelola state dan validasi input data siswa

---
+++ app/src/main/java/com/example/roomsatu/viewmodel/EntryViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.roomsatu.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsatu.repositori.RepositoriSiswa
+import com.example.roomsatu.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(uiState = detailSiswa))
+    }
+
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(siswa = uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+
+    data class UIStateSiswa(
+        val detailSiswa: DetailSiswa = DetailSiswa(),
+        val isEntryValid: Boolean = false
+    )
+
+    data class DetailSiswa(
+        val id: Int = 0,
+        val nama: String = "",
+        val alamat: String = "",
+        val telpon: String = ""
+    )
+
+    fun DetailSiswa.toSiswa(): Siswa = Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+
+    fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+    fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+}
