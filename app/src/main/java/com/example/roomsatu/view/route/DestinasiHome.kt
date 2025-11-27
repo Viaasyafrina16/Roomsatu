d4d1077c9a25f4634b6cd5dd4067848c9f5bec65
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-27 11:45:44 +0700

    memanggil id string Data Siswa

--- app/src/main/java/com/example/roomsatu/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/roomsatu/view/route/DestinasiHome.kt
@@ -5,5 +5,5 @@ import com.example.roomsatu.R
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
 
-    override  val titleRes = "Data Siswa"
+    override  val titleRes= R.string.app_name
 }
